edfd30a1213af19d8696e7a38eb097754f939911
Comunicando Igrejas <comunicandoigrejas@gmail.com>
2026-05-12 09:49:35 -0300

    Update 8_Datas_Comemorativas.py

--- pages/8_Datas_Comemorativas.py
+++ pages/8_Datas_Comemorativas.py
@@ -1,9 +1,13 @@
-# modules/tema_comemorativo.py
 import streamlit as st
+
+st.set_page_config(page_title="Datas Comemorativas", layout="wide", page_icon="🎉")
+
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 def exibir():
     st.markdown("<h1 class='gradient-title'>🎉 Datas Comemorativas</h1>", unsafe_allow_html=True)
-    st.markdown("<p class='sub-title'>Templates para Natal, Páscoa, Ano Novo, Dia das Mães, Pais e Feriados.</p>", unsafe_allow_html=True)
+    st.markdown("<p class='sub-title'>Templates para Natal, Páscoa, Ano Novo, Dia das Mães e Pais.</p>", unsafe_allow_html=True)
     st.markdown("<br>", unsafe_allow_html=True)
 
     col1, col2 = st.columns(2)
@@ -18,9 +22,11 @@ def exibir():
 
     with col2:
         st.markdown("<div class='premium-card'>", unsafe_allow_html=True)
-        st.markdown("**Pack #02 - Ano Novo**")
+        st.markdown("**Pack Ano Novo**")
         st.write("Disponível em breve!")
         st.markdown("</div>", unsafe_allow_html=True)
 
     st.markdown("---")
     st.info("🔄 Novos packs são adicionados semanalmente!")
+
+exibir()
